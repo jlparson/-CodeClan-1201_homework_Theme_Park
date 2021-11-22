7127b94324224564483d8240cb09352d41ecf46f
Jenny Parson <jlparson@hotmail.com>
2021-11-22 18:02:38 +0000

    RollerCoaster min height and age added

--- src/test/java/attractions/RollercoasterTest.java
+++ src/test/java/attractions/RollercoasterTest.java
@@ -2,16 +2,22 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class RollercoasterTest {
 
     RollerCoaster rollerCoaster;
+    Visitor visitor1;
+    Visitor visitor2;
 
     @Before
     public void setUp() {
+
         rollerCoaster = new RollerCoaster("Blue Ridge", 10);
+        visitor1 = new Visitor(10, 125, 6.00);
+        visitor2 = new Visitor(19, 155, 25.00);
     }
 
     @Test
@@ -27,5 +33,15 @@ public class RollercoasterTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, rollerCoaster.getVisitCount());
+    }
+
+    @Test
+    public void shouldCheckIsAllowedToTrue(){
+        assertEquals(true, rollerCoaster.isAllowedTo(visitor2));
+    }
+
+    @Test
+    public void shouldCheckIsAllowedToFalse(){
+        assertEquals(false, rollerCoaster.isAllowedTo(visitor1));
     }
 }
