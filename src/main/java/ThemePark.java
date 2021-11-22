f3905e5c14da26a1e1a199128787584a6c6bf950
Jenny Parson <jlparson@hotmail.com>
2021-11-22 20:49:46 +0000

    set up ThemePark extension

--- src/main/java/ThemePark.java
+++ src/main/java/ThemePark.java
@@ -1,2 +1,18 @@
+import attractions.Attraction;
+import behaviours.IReviewed;
+import stalls.Stall;
+
+import java.util.ArrayList;
+
 public class ThemePark {
+
+    private ArrayList<Attraction> attractions;
+    private ArrayList<Stall> stalls;
+    private ArrayList<IReviewed> reviewedObjects;
+
+    public ThemePark(){
+        this.attractions = new ArrayList<>();
+        this.stalls = new ArrayList<>();
+        this.reviewedObjects = new ArrayList<>();
+    }
 }
