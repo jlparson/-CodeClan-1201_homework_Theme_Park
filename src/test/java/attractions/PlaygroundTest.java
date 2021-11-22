518d93f8cd5c9fbab16e0620d6ddd191e05035d5
Jenny Parson <jlparson@hotmail.com>
2021-11-22 17:43:43 +0000

    added playground ISecurity & testing

--- src/test/java/attractions/PlaygroundTest.java
+++ src/test/java/attractions/PlaygroundTest.java
@@ -2,15 +2,20 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class PlaygroundTest {
     Playground playground;
+    Visitor visitor1;
+    Visitor visitor2;
 
     @Before
     public void setUp() throws Exception {
         playground = new Playground("Fun Zone", 7);
+        visitor1 = new Visitor(13, 135, 5.00);
+        visitor2 = new Visitor(25, 155, 20.00);
     }
 
     @Test
@@ -26,5 +31,15 @@ public class PlaygroundTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, playground.getVisitCount());
+    }
+
+    @Test
+    public void shouldCheckIsAllowedToTrue(){
+        assertEquals(true, playground.isAllowedTo(visitor1));
+    }
+
+    @Test
+    public void shouldCheckIsAllowedToFalse(){
+        assertEquals(false, playground.isAllowedTo(visitor2));
     }
 }
